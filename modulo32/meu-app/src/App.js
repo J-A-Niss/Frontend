59131b01cd69bc14cd4122f72d50af51c44ce664
Joao Avelas <jplinkas@gmail.com>
2022-12-02 21:49:38 -0300

    code-together style guide

--- modulo32/meu-app/src/App.js
+++ modulo32/meu-app/src/App.js
@@ -2,14 +2,16 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import './App.css';
-import Menu from './components/Menu';
+// import Menu from './components/Menu';
+import SignInSide from './components/SignInSide/SignInSide';
 
 
 function App() {
   return (
     <div className='App'>
-      <Button variant="contained">Hello World!</Button>
-      <Menu />
+      {/* <Button variant="contained">Hello World!</Button> */}
+      {/* <Menu /> */}
+      <SignInSide />
     </div>
   );
 }
